54d7646337e784916b5fce8019091e84543395fc
ruslana <ruslana.prus@ukr.net>
2025-01-28 00:41:50 +0100

    add exception

--- src/main/java/com/example/notemanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/notemanager/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.example.notemanager.exception;
 
+import com.example.notemanager.notes.exception.NoteServiceException;
 import com.example.notemanager.users.exception.UserServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,9 +15,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(value = UserServiceException.class)
+    @ExceptionHandler({NoteServiceException.class, UserServiceException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleUserServiceException(UserServiceException exception, Model model) {
+    public String handleDomainSpecificExceptions(Exception exception, Model model) {
         log.error("A domain-specific issue was detected: {}", exception.getMessage(), exception);
         model.addAttribute("message", exception.getMessage());
         return "note/error";
@@ -27,6 +29,14 @@ public class GlobalExceptionHandler {
         log.error("Unexpected error occurred: {}", exception.getMessage(), exception);
         model.addAttribute("message", "Internal Server Error");
         return "note/error";
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String handleAuthenticationException(AuthenticationException exception, Model model) {
+        log.error("Authentication failed: {}", exception.getMessage());
+        model.addAttribute("message", "Invalid username or password");
+        return "login";
     }
 
 }
